8d9ae603225de8e851c52c908fd32aea7f81eb1b
Kiran Kui <yk2959@cumc.columbia.edu>
2022-09-22 11:47:44 -0400

    mutate!!

--- data_wrangling_lecture_2.R
+++ data_wrangling_lecture_2.R
@@ -8,26 +8,37 @@ options(tibble.print_min = 3)
 #load litters data
 litters_data = read_csv("./data/FAS_litters.csv",col_types = "ccddiiii")
 
-#Clean names from janitor makes the names nice and neat
+#CLEAN NAMES from janitor makes the names nice and neat
 litters_data = janitor::clean_names(litters_data)
+
 
 pups_data = read_csv("./data/FAS_pups.csv",
                      col_types = "ciiiii")
 pups_data = janitor::clean_names(pups_data)
 
-
+# SELECT: choose some columns and not others 
 select(litters_data, group, litter_number, gd0_weight, pups_born_alive)
 
 select(litters_data, group:gd_of_birth)
 
+##get rid of pups survive column 
 select(litters_data, -pups_survive)
 
+#RENAME the column numbers NEW NAME=old name
 rename(litters_data, GROUP = group, LiTtEr_NuMbEr = litter_number)
 
+###########################################
+# ?select_helpers to help 
+############################################
+
+
+#SELECT everything that starts with "gd"
 select(litters_data, starts_with("gd"))
 
+#SELECT litter number, pups_survive as the first few columns but keep everything else!
 select(litters_data, litter_number, pups_survive, everything())
 
+#RELOCATE does the same as above, but you don't have to say everything!
 relocate(litters_data, litter_number, pups_survive)
 
 ###FILTER THE LITTERS DATA 
@@ -42,8 +53,26 @@ relocate(litters_data, litter_number, pups_survive)
 #drop_na(litters_data): remove any rows with a missing value
 #drop_na(litters_data, wt_increase): remove rows for which wt_increase is missing
 
-mutate(litters_data,
-       wt_gain = gd18_weight - gd0_weight,group = str_to_lower(group))
+
+
+#FILTER: remove rows or keep rows for subsequent analysis BUT you have to GIVE IT SOME CONDITION [<, >, = ] using logical operators!!!
+
+filter(litters_data, gd0_weight <22)
+
+filter(litters_data, gd0_weight >=22)
+
+filter(litters_data, gd0_weight ==22) #note 2 equal signs 
+
+filter(litters_data, gd0_weight !=22) #not equal to "!="
+
+filter(litters_data, gd0_weight >=22, gd_of_birth == 20) 
+
+filter(litters_data, group=="Mod8")
+
+filter(litters_data, group %>% c("Con7","Mod8")) # group in Con6 and Mod8
+
+#MUTATE: modify some variable + create new variable
+mutate(litters_data, wt_gain = gd18_weight - gd0_weight,group = str_to_lower(group)) #new variable called "wt_gain" + make the group variable to LOWERCASE, new variables at the end!!
 
 head(arrange(litters_data, group, pups_born_alive), 10)
 
